4012850d866d908960ff0fd5459a004189dbb28f
Jeff Nygren <jnygren.programmer@gmail.com>
2015-09-02 21:25:54 -0500

    Added Login/Logout.

--- Badge/HTPage.cs
+++ Badge/HTPage.cs
@@ -5,7 +5,21 @@ namespace Badge
 {
     public class HTPage
     {
-        public static By EmpLogin = By.Id("login-text");
-        public static By SpouseLogin = By.Id("spouse-text");
+        // Home page
+        public static By BtnEmpLogin = By.Id("login-text");
+        public static By BtnSpouseLogin = By.Id("spouse-text");
+
+        // Login page
+        public static By TxtUsername = By.Id("username");
+        public static By TxtPassword = By.Id("passwd");
+        public static By BtnLogin = By.Id("login-btn");
+
+        // Header
+        public static By DDUserLinks = By.Id("user-links-trigger");
+
+        // Dropdown
+        public static By LinkLogout = By.XPath("//a[@href='/logout/']");
+
+
     }
 }
--- Badge/HealtherTogether.cs
+++ Badge/HealtherTogether.cs
@@ -7,7 +7,9 @@ namespace Badge
     class HealtherTogether
     {
         private static IWebDriver driver = null;
-        private static readonly string HealtherTogetherURL = "http://www.medtronichealthiertogether.com/members/healthyhabits/3";
+        private static readonly string HealtherTogetherURL = "https://www.medtronichealthiertogether.com/";
+        private static readonly string TEMP_Username = "JNygren";
+        private static readonly string TEMP_Password = "VMN6p*B6";
 
 
         public HealtherTogether()
@@ -26,7 +28,14 @@ namespace Badge
             {
                 driver.Url = HealtherTogetherURL;
 
-                driver.FindElement(HTPage.SpouseLogin).Click();
+                // Home Page
+                driver.FindElement(HTPage.BtnSpouseLogin).Click();
+
+                // Login Page
+                IWebElement btnLogin = driver.FindElement(HTPage.BtnLogin);
+                driver.FindElement(HTPage.TxtUsername).SendKeys(TEMP_Username);
+                driver.FindElement(HTPage.TxtPassword).SendKeys(TEMP_Password);
+                btnLogin.Click();
             }
 
             return result;
@@ -37,10 +46,14 @@ namespace Badge
         {
             if (driver != null)
             {
+                IWebElement dd = driver.FindElement(HTPage.DDUserLinks);
+                dd.Click();
+                driver.FindElement(HTPage.LinkLogout).Click();
 
                 driver.Quit();
                 driver = null;
             }
         }
+
     }
 }
